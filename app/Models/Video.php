6679ee747f18a14ca7eaba53a9a6f3b1610c1c8d
Fabio Acosta <Fabio@Acosta.com>
2025-01-10 21:07:05 +0000

    Relaciones entre los modelos creadas

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -9,4 +9,9 @@ class Video extends Model
 {
     /** @use HasFactory<\Database\Factories\VideoFactory> */
     use HasFactory;
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
 }
